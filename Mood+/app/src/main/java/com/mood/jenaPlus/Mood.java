e5ca668c6d7cd73c14a0c72dfb54021c5ba9ad1c
Aquafina99 <xxiang1@ualberta.ca>
2017-02-25 14:03:22 -0700

    updated

---
+++ Mood+/app/src/main/java/com/mood/jenaPlus/Mood.java
@@ -0,0 +1,10 @@
+package com.mood.jenaPlus;
+
+/**
+ * Created by ceciliaxiang on 2017-02-25.
+ */
+
+public class Mood {
+
+
+}
---
+++ Mood+/app/src/main/java/com/mood/jenaPlus/User.java
@@ -0,0 +1,17 @@
+package com.mood.jenaPlus;
+
+/**
+ * Created by ceciliaxiang on 2017-02-25.
+ */
+
+public abstract class User {
+
+
+    protected String userName;
+
+    public String getUserName(){
+        return userName;
+    }
+
+
+}
