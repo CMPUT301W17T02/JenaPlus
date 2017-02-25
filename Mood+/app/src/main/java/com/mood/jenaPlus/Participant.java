03bf786919d87d7b56ca3deae2a521295457ea53
Julienne Belardo <belardo@ualberta.ca>
2017-02-25 15:31:54 -0700

    init participant

---
+++ Mood+/app/src/main/java/com/mood/jenaPlus/Participant.java
@@ -0,0 +1,20 @@
+package com.mood.jenaPlus;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ceciliaxiang on 2017-02-25.
+ */
+
+public class Participant extends User {
+
+    private String userName;
+    private ArrayList<Mood> moodList;
+    private ArrayList<Mood> followingMoodList;
+
+
+
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Mood+
+CMPUT301 T02 Group Project
