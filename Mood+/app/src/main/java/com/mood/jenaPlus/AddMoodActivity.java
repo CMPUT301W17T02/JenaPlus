7ece9d5e9e90d93a0754e7d3b3af8ebf54c41cef
carrolji <jirakul@ualberta.ca>
2017-02-25 16:54:07 -0700

    Welcome page

---
+++ Mood+/app/src/main/java/com/mood/jenaPlus/AddMoodActivity.java
@@ -0,0 +1,17 @@
+package com.mood.jenaPlus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by carrotji on 2017-02-25.
+ */
+
+public class AddMoodActivity extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_mood_interface);
+    }
+}
--- Mood+/app/src/main/java/com/mood/jenaPlus/moodPlusActivity.java
+++ Mood+/app/src/main/java/com/mood/jenaPlus/moodPlusActivity.java
@@ -1,5 +1,6 @@
 package com.mood.jenaPlus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -40,6 +41,8 @@ public class moodPlusActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Intent intent = getIntent();
     }
 
     @Override
