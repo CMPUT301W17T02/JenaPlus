b57aaf27b52d7c389dd8708d6b2503c2a15418d4
Aquafina99 <xxiang1@ualberta.ca>
2017-02-25 17:21:16 -0700

    addMood method

--- Mood+/app/src/main/java/com/mood/jenaPlus/MoodList.java
+++ Mood+/app/src/main/java/com/mood/jenaPlus/MoodList.java
@@ -12,6 +12,13 @@ public class MoodList {
     private ArrayList<Mood> FollowingMoodList = new ArrayList<>();
 
 
+    public void addMood(Mood mood){
+        
+        UserMoodList.add(mood);
+
+    }
+
+
     public Mood getUserIndex(int index){
         return UserMoodList.get(index);
     }
