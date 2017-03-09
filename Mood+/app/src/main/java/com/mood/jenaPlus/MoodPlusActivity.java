f22f3e0b291e003b6d8d7531c8e69bd3bd825ce5
carrolji <jirakul@ualberta.ca>
2017-03-08 18:50:55 -0700

    add person icon

--- Mood+/app/src/main/java/com/mood/jenaPlus/MoodPlusActivity.java
+++ Mood+/app/src/main/java/com/mood/jenaPlus/MoodPlusActivity.java
@@ -87,7 +87,7 @@ public class MoodPlusActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_following) {
             return true;
         }
 
