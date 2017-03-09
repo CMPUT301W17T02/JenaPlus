8a26ca70e07296714f2b42406885c0abaf9bccca
carrolji <jirakul@ualberta.ca>
2017-03-08 19:32:42 -0700

    add icons

--- Mood+/app/src/main/java/com/mood/jenaPlus/MoodPlusActivity.java
+++ Mood+/app/src/main/java/com/mood/jenaPlus/MoodPlusActivity.java
@@ -103,14 +103,8 @@ public class MoodPlusActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+            
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
