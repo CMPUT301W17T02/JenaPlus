1c83d02a337e306a43f9383a7402b0614d032b15
Aquafina99 <xxiang1@ualberta.ca>
2017-03-10 19:34:43 -0700

    editMood

---
+++ Mood+/app/src/main/java/com/mood/jenaPlus/EditMoodActivity.java
@@ -0,0 +1,49 @@
+package com.mood.jenaPlus;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class EditMoodActivity extends AppCompatActivity {
+
+    private Button addButton;
+    private EditText message;
+    private Button socialPopup;
+    private GridView gridview;
+    private ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_mood_interface);
+
+    }
+
+}
