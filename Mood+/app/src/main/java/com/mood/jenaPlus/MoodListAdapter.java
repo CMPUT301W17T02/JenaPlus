d02f3e52234e1d1ff64e1a93ab673114c7ca9b7b
carrolji <jirakul@ualberta.ca>
2017-03-12 18:57:58 -0600

    moodListAdapter created

---
+++ Mood+/app/src/main/java/com/mood/jenaPlus/MoodListAdapter.java
@@ -0,0 +1,41 @@
+package com.mood.jenaPlus;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by carrotji on 2017-03-12.
+ */
+
+public class MoodListAdapter extends ArrayAdapter<Mood> {
+
+    public MoodListAdapter(Context context, ArrayList<Mood> moodList){
+        super(context,0,moodList);
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent){
+
+        Mood moodList = getItem(position);
+
+        if (view == null)
+            view = LayoutInflater.from(getContext()).inflate(R.layout.mood_plus_listview,parent,false);
+
+        TextView dateText = (TextView) view.findViewById(R.id.date);
+        TextView messageText = (TextView) view.findViewById(R.id.message);
+        ImageView moodIconImage = (ImageView) view.findViewById(R.id.moodIcon);
+
+        dateText.setText(moodList.getDateString());
+        messageText.setText(moodList.getText());
+
+
+        return view;
+    }
+}
