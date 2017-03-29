596c35b63e186a2544dbc7237d9770b50284791f
carrolji <jirakul@ualberta.ca>
2017-03-28 18:35:26 -0600

    passing array of locations

--- Mood+/app/src/main/java/com/mood/jenaPlus/MarkerActivity.java
+++ Mood+/app/src/main/java/com/mood/jenaPlus/MarkerActivity.java
@@ -1,8 +1,11 @@
 package com.mood.jenaPlus;
 
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -12,19 +15,18 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 public class MarkerActivity extends FragmentActivity implements
         GoogleMap.OnMarkerClickListener,
         OnMapReadyCallback {
 
-    private static final LatLng PERTH = new LatLng(-31.952854, 115.857342);
-    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
-    private static final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
-
-    private Marker mPerth;
-    private Marker mSydney;
-    private Marker mBrisbane;
+    private Marker testMarker;
 
     private GoogleMap mMap;
+
+    ArrayList<Marker> markers;
+    ArrayList<LatLng> arrayLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +36,7 @@ public class MarkerActivity extends FragmentActivity implements
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
     /** Called when the map is ready. */
@@ -41,21 +44,21 @@ public class MarkerActivity extends FragmentActivity implements
     public void onMapReady(GoogleMap map) {
         mMap = map;
 
+        Intent intent = getIntent();
+        arrayLocation = intent.getParcelableArrayListExtra("longLat_dataProvider");
+
         // Add some markers to the map, and add a data object to each marker.
-        mPerth = mMap.addMarker(new MarkerOptions()
+        for(LatLng location: arrayLocation) {
+            Log.i("LOCATIONNNNNN","Contents of arrayLocation: " + location);
+            testMarker = mMap.addMarker(new MarkerOptions().position(location).title("TESTING"));
+            testMarker.setTag(0);
+        }
+
+        // Add some markers to the map, and add a data object to each marker.
+     /*   mPerth = mMap.addMarker(new MarkerOptions()
                 .position(PERTH)
                 .title("Perth"));
-        mPerth.setTag(0);
-
-        mSydney = mMap.addMarker(new MarkerOptions()
-                .position(SYDNEY)
-                .title("Sydney"));
-        mSydney.setTag(0);
-
-        mBrisbane = mMap.addMarker(new MarkerOptions()
-                .position(BRISBANE)
-                .title("Brisbane"));
-        mBrisbane.setTag(0);
+        mPerth.setTag(0);*/
 
         // Set a listener for marker click.
         mMap.setOnMarkerClickListener(this);
