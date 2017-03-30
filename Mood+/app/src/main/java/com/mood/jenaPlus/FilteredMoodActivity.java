032e5743af0ebf90b91ca5a8bd7d22426f59db3b
carrolji <jirakul@ualberta.ca>
2017-03-29 22:17:01 -0600

    view location your own mood done

--- Mood+/app/src/main/java/com/mood/jenaPlus/FilteredMoodActivity.java
+++ Mood+/app/src/main/java/com/mood/jenaPlus/FilteredMoodActivity.java
@@ -144,9 +144,6 @@ public class FilteredMoodActivity extends AppCompatActivity implements MPView<Mo
             }
         }
 
-
-
-
         TextView test = (TextView) findViewById(R.id.test_string);
         String who = "Mood: " + moodString;
         //test.setText(who);
